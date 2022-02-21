38d2bc600ef1282acfc63960095861170004cba9
Steven Kalt <kalt.steven@gmail.com>
2022-02-21 18:20:52 -0500

    feat: add column, table support by database as views

--- pkg/schema/views.sql
+++ pkg/schema/views.sql
@@ -60,3 +60,76 @@ CREATE VIEW cols AS
     ) AS col
   GROUP BY col.column_id, col.note, col.column_type
   ORDER BY table_name, column_name, db_name, 7; -- 7 = versions
+
+CREATE VIEW column_support AS
+  SELECT
+      table_name
+    , column_name
+    , group_concat(distinct db_name)
+  FROM  (
+      SELECT
+          cv.id
+        , db.name "db_name"
+        , v.version "version"
+        , t.name table_name
+        , col.name column_name
+        , col.id column_id
+        , column_type.name column_type
+        , cv.nullable
+        , note.note
+        , link.id url_id
+        , v.is_current
+        , license.license
+        , license.url_id AS license_url_id
+        , license.attribution
+      FROM column_versions AS cv
+      JOIN versions AS v ON v.id = cv.version_id
+      JOIN dbs AS db ON db.id = v.db_id
+      JOIN columns AS col ON col.id = cv.column_id
+      JOIN tables AS t ON t.id = col.table_id
+      LEFT OUTER JOIN types AS column_type ON column_type.id = cv.type_id
+      -- ^ sometimes scraped docs don't include types
+      LEFT OUTER JOIN urls AS link ON cv.url_id = link.id
+      -- ^ observed columns don't come with reference urls
+      LEFT OUTER JOIN notes AS note ON cv.note_id = note.id
+      LEFT OUTER JOIN licenses AS license ON cv.note_license_id = license.id
+      ORDER BY 4, 5, 2, cast(v.version AS REAL) DESC, v.version
+    )
+  GROUP BY table_name, column_name
+  ORDER BY table_name, column_name;
+  
+CREATE VIEW relation_support AS
+  SELECT
+      table_name
+    , group_concat(distinct db_name)
+  FROM  (
+      SELECT
+          cv.id
+        , db.name "db_name"
+        , v.version "version"
+        , t.name table_name
+        , col.name column_name
+        , col.id column_id
+        , column_type.name column_type
+        , cv.nullable
+        , note.note
+        , link.id url_id
+        , v.is_current
+        , license.license
+        , license.url_id AS license_url_id
+        , license.attribution
+      FROM column_versions AS cv
+      JOIN versions AS v ON v.id = cv.version_id
+      JOIN dbs AS db ON db.id = v.db_id
+      JOIN columns AS col ON col.id = cv.column_id
+      JOIN tables AS t ON t.id = col.table_id
+      LEFT OUTER JOIN types AS column_type ON column_type.id = cv.type_id
+      -- ^ sometimes scraped docs don't include types
+      LEFT OUTER JOIN urls AS link ON cv.url_id = link.id
+      -- ^ observed columns don't come with reference urls
+      LEFT OUTER JOIN notes AS note ON cv.note_id = note.id
+      LEFT OUTER JOIN licenses AS license ON cv.note_license_id = license.id
+      ORDER BY 4, 5, 2, cast(v.version AS REAL) DESC, v.version
+    )
+  GROUP BY table_name
+  ORDER BY table_name;
