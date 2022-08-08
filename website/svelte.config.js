168cbcec9942650f5611831316de035502cab209
Steven Kalt <kalt.steven@gmail.com>
2022-08-07 20:03:10 -0400

    ...

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-auto";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,7 +9,7 @@ const config = {
 
   kit: {
     prerender: {
-      default: true,
+      // default: true,
       concurrency: 1,
     },
     adapter: adapter(),
